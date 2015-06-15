55b89541cdf7a0e5e17d9d5a10707066607c7570
Diobox <a@diobox.com>
2015-06-15 16:31:48 -0400

    add swagger docs build

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -102,6 +102,28 @@ private
       end
     end
   end
+  
+  def run_swagger_docs_rake_task
+    instrument 'ruby.run_swagger_docs_rake_task' do
+
+     docs = rake.task("swagger:docs")
+     return true unless docs.is_defined?
+
+     topic "Generating Swagger documentation"
+     docs.invoke(env: rake_env)
+     if docs.success?
+       puts "Swagger documentation generation completed (#{"%.2f" % docs.time}s)"
+     else
+       swagger_docs_fail(docs.output)
+     end
+   end
+  end
+
+  def swagger_docs_fail(output)
+    log "swagger_docs", :status => "failure"
+    msg = "Generating Swagger documentation failed.\n"
+    error msg
+  end
 
   def rake_env
     if user_env_hash.empty?
